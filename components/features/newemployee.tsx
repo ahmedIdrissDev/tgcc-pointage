ace75333f4ac5b0b25a46429cf3f6ade4101ebe7
AhmedIdrissDev <webocamw224@gmail.com>
2026-01-06 11:21:33 +0100

    fix and push

--- components/features/newemployee.tsx
+++ components/features/newemployee.tsx
@@ -14,23 +14,12 @@ const Newemployee = () => {
   return (
 <Drawer>
   <DrawerTrigger>
-     <button className="button shadow-none border-0 rounded-md w-40 h-11 bg-tgcc-500 text-white">
+     <button className="button shadow-none border-0 rounded-md w-40 h-11 bg-tgcc-600 text-white">
          <Plus/>
         Nouvel employé</button>
   </DrawerTrigger>
-  <DrawerContent className='h-[120dvh] '>
-    <DrawerHeader>
-      <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-      <DrawerDescription>This action cannot be undone.</DrawerDescription>
-    </DrawerHeader>
-    <DrawerFooter>
- <button className="button shadow-none border-0 rounded-md w-40 h-11 bg-tgcc-500 text-white">
-         <Plus/>
-        Nouvel employé</button>      <DrawerClose>
- <button className="button shadow-none border-0 rounded-md w-40 h-11 bg-tgcc-500 text-white">
-         <Plus/>
-        Nouvel employé</button>      </DrawerClose>
-    </DrawerFooter>
+  <DrawerContent className='h-[190dvh] rounded-md bg-white '>
+    
   </DrawerContent>
 </Drawer>  )
 }
